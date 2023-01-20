01258938bb76ae80b5bf9b244334ea9d7510208f
Jason Hwang <173634806+dotjasonhwang@users.noreply.github.com>
2023-01-19 22:40:30 -0600

    Default to uppercase

--- src/my_argparse.py
+++ src/my_argparse.py
@@ -2,5 +2,5 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(description='Create a lyrics ppt')
-    parser.add_argument('-u', '--uppercase', action=argparse.BooleanOptionalAction)
+    parser.add_argument('-u', '--uppercase', default=True, action=argparse.BooleanOptionalAction)
     return parser.parse_args()
